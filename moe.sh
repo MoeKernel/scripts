2cfcffe52e7dc8258889f4918c3affbedacc4a4f
明 <akariondev@gmail.com>
2024-01-06 20:10:30 -0300

    Update moe.sh

--- moe.sh
+++ moe.sh
@@ -99,7 +99,6 @@ if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
     rm -rf out/arch/arm64/boot
     echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
     echo "Zip: $ZIPNAME"
-    # curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
 else
     echo -e "\nCompilation failed!"
     exit 1
