555a42b8cbaeeb0b9810f7a54f41287cfa455555
明 <akariondev@gmail.com>
2024-05-16 00:25:44 -0300

    ksu_update.sh: update

--- ksu_update.sh
+++ ksu_update.sh
@@ -39,10 +39,3 @@ else
 fi
 
 curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | $cmd
-
-# Remove KPROBES dependency warning 
-# if option is set to "y"
-# Default: y (yes)
-# if [ "$remove_kprobes_warning" = "y" ]; then
-#   sed -i '59,60d' KernelSU/kernel/ksu.c
-# fi
