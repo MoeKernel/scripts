8e349d020669846805df4af78d39958c5d7ff587
Shoiya Akari <akariondev@gmail.com>
2024-09-24 22:20:14 -0300

    Update moe.sh

--- moe.sh
+++ moe.sh
@@ -1,103 +1,75 @@
 #!/bin/bash
-# Compile script for MoeKernel
-# Copyright (C) 2020-2021 Adithya R.
+# Compile script for Moeニャン kernel
+# Copyright (C) 2020-2021 Adithya R. | (C) 2024 - Shoiya Akari.
 
 SECONDS=0
-ZIPNAME="MoeKSU-$(date '+%Y%m%d').zip"
-TC_DIR="$HOME/tc/clang-20.0.0"
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-15.0"
-GCC_32_DIR="$HOME/tc/arm-linux-androideabi-15.0"
-AK3_DIR="$HOME/android/AnyKernel3"
-DEFCONFIG="vendor/moe_defconfig"
+TC_DIR="$HOME/tc/clang-r450784d"
+AK3_DIR="$HOME/AnyKernel3"
+DEFCONFIG="lisa_defconfig"
+ZIPNAME="MoeKernel-lisa-$(date '+%Y%m%d-%H%M').zip"
+
+if ! [ -d "${TC_DIR}" ]; then
+echo "Clang not found! Cloning to ${TC_DIR}..."
+if ! git clone --depth=1 https://gitlab.com/tejas101k/clang-r450784d.git ${TC_DIR}; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+
+MAKE_PARAMS="O=out ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- LLVM=1 LLVM_IAS=1 \
+	CROSS_COMPILE=$TC_DIR/bin/llvm-"
 
 export PATH="$TC_DIR/bin:$PATH"
 
-export KBUILD_BUILD_USER=Moe
-export KBUILD_BUILD_HOST=Nyan
-
-if ! [ -d "${TC_DIR}" ]; then
-    echo "Clang not found! Cloning to ${TC_DIR}..."
-    if ! git clone --depth=1 https://gitlab.com/moehacker/clang-r498229b ${TC_DIR}; then
-        echo "Cloning failed! Aborting..."
-        exit 1
-    fi
-fi
-
-if ! [ -d "${GCC_64_DIR}" ]; then
-    echo "gcc not found! Cloning to ${GCC_64_DIR}..."
-    if ! git clone --depth=1 -b 15 https://github.com/whyakari/aarch64-zyc-linux-gnu ${GCC_64_DIR}; then
-        echo "Cloning failed! Aborting..."
-        exit 1
-    fi
-fi
-
-if ! [ -d "${GCC_32_DIR}" ]; then
-    echo "gcc_32 not found! Cloning to ${GCC_32_DIR}..."
-    if ! git clone --depth=1 -b 15 https://github.com/whyakari/arm-zyc-linux-gnueabi ${GCC_32_DIR}; then
-        echo "Cloning failed! Aborting..."
-        exit 1
-    fi
-fi
-
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
-    make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-    cp out/defconfig arch/arm64/configs/$DEFCONFIG
-    exit
+	make $MAKE_PARAMS $DEFCONFIG savedefconfig
+	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
+	exit
 fi
 
 if [[ $1 = "-c" || $1 = "--clean" ]]; then
-    rm -rf out
+	rm -rf out
+	echo "Cleaned output folder"
 fi
 
-if [[ $1 = "-m" || $1 = "--menu" ]]; then
-    mkdir -p out
-    make O=out ARCH=arm64 $DEFCONFIG menuconfig
-elif [[ $1 = "menu" ]]; then
-    mkdir -p out
-    make O=out ARCH=arm64 $DEFCONFIG menuconfig
-else
-    mkdir -p out
-    make O=out ARCH=arm64 $DEFCONFIG
+mkdir -p out
+make $MAKE_PARAMS $DEFCONFIG
+
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) $MAKE_PARAMS || exit $?
+make -j$(nproc --all) $MAKE_PARAMS INSTALL_MOD_PATH=modules INSTALL_MOD_STRIP=1 modules_install
+
+kernel="out/arch/arm64/boot/Image"
+dtb="out/arch/arm64/boot/dts/vendor/qcom/yupik.dtb"
+dtbo="out/arch/arm64/boot/dts/vendor/qcom/lisa-sm7325-overlay.dtbo"
+
+if [ ! -f "$kernel" ] || [ ! -f "$dtb" ] || [ ! -f "$dtbo" ]; then
+	echo -e "\nCompilation failed!"
+	exit 1
 fi
 
-echo -e "\nStarting compilation... wait\n"
-make -j$(nproc --all) \
-    O=out \
-    ARCH=arm64 \
-    CC="ccache clang" \
-    LD=ld.lld \
-    AR=llvm-ar \
-    AS=llvm-as \
-    NM=llvm-nm \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip \
-    CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- \
-    CLANG_TRIPLE=aarch64-linux-gnu- \
-    Image.gz-dtb dtbo.img
-
-if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && \
-   [ -f "out/arch/arm64/boot/dtbo.img" ]; then
-    echo -e "\nKernel compiled successfully! Zipping up...\n"
-    if [ -d "$AK3_DIR" ]; then
-        cp -r $AK3_DIR AnyKernel3
-    elif ! git clone -q https://github.com/MoeKernel/AnyKernel3; then
-        echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
-        exit 1
-    fi
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
-    cp out/arch/arm64/boot/dtbo.img AnyKernel3
-    rm -f *zip
-    cd AnyKernel3
-    git checkout master &> /dev/null
-    zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
-    cd ..
-    rm -rf AnyKernel3
-    rm -rf out/arch/arm64/boot
-    echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-    echo "Zip: $ZIPNAME"
-else
-    echo -e "\nCompilation failed!"
-    exit 1
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -d "$AK3_DIR" ]; then
+	cp -r $AK3_DIR AnyKernel3
+	git -C AnyKernel3 checkout lisa &> /dev/null
+elif ! git clone -q https://github.com/MoeKernel/AnyKernel3 -b lisa; then
+	echo -e "\nAnyKernel3 repo not found locally and couldn't clone from GitHub! Aborting..."
+	exit 1
 fi
+
+cp $kernel AnyKernel3
+cp $dtb AnyKernel3/dtb
+python2 scripts/dtc/libfdt/mkdtboimg.py create AnyKernel3/dtbo.img --page_size=4096 $dtbo
+cp $(find out/modules/lib/modules/5.4* -name '*.ko') AnyKernel3/modules/vendor/lib/modules/
+cp out/modules/lib/modules/5.4*/modules.{alias,dep,softdep} AnyKernel3/modules/vendor/lib/modules
+cp out/modules/lib/modules/5.4*/modules.order AnyKernel3/modules/vendor/lib/modules/modules.load
+sed -i 's/\(kernel\/[^: ]*\/\)\([^: ]*\.ko\)/\/vendor\/lib\/modules\/\2/g' AnyKernel3/modules/vendor/lib/modules/modules.dep
+sed -i 's/.*\///g' AnyKernel3/modules/vendor/lib/modules/modules.load
+rm -rf out/arch/arm64/boot out/modules
+cd AnyKernel3
+zip -r9 "../$ZIPNAME" * -x .git README.md *placeholder
+cd ..
+rm -rf AnyKernel3
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "Zip: $ZIPNAME"
